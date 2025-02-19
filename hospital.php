34e33fcbc6e0166ed3a5f115a4b7a1a2906b5c80
osamahalshameri <osamahassistant@gmail.com>
2025-02-19 20:16:51 +0300

    first commit

--- hospital.php
+++ hospital.php
@@ -8,29 +8,21 @@
 <body>
     <?php
     $coon= mysqli_connect('localhost','root','','hospital');
-    if($coon){
-        echo "تم الاتصال بنجاح";
-    }else{
-        echo "لم يتم الاتصال";
-    }
-    //ادخال
-   // $coon->query("INSERT INTO doctors(id,name,specialty) VALUES (1,'Osamah','cardiology')");
+   // $coon->query("INSERT INTO doctors(id,name,specialty) VALUES (1,'mohammed','cardiology')");
    // $coon->query("INSERT INTO doctors(id,name,specialty) VALUES (2,'Ahmed','cardiology')");
    // $coon->query("INSERT INTO patient(id,name,age,doctor_id) VALUES (1,'omar',30,0)");
-//تعديل
+      // $coon->query("INSERT INTO patient(id,name,age,doctor_id) VALUES (1,'omar',30,0)");
+   // $coon->query("INSERT INTO patient(id,name,age,doctor_id) VALUES (2,'ali',40,0)");
    //$coon->query("UPDATE patient SET doctor_id=1 WHERE id=1");
-   //حذف
-  // $coon->query("DELETE FROM doctors WHERE id=2");
-//طباعة الجدول الاول
+   $coon->query("DELETE FROM doctors WHERE id=1");
 $rseult = $coon->query("SELECT * FROM doctors");
-echo "<h1>Doctors</h1><table border='1'><tr><th>Id</th><th>Name</th><th>Specialty</th><br/>";
+echo "Doctors<table border='1'><tr><th>Id</th><th>Name</th><th>Specialty</th><br/>";
 while($row = $rseult->fetch_assoc()){
     echo"<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['specialty']}</td></tr>";
 }
-   echo"</table>.</br>";
-//طباعة الجدول الثاني
+   echo"</table>";
 $rseult1 = $coon->query("SELECT * FROM patient ");
-echo "<h1>Patient</h1><table border='1'><tr><th>Id</th><th>Name</th><th>agey</th><th>doctor_id</th><br/>";
+echo "Patient<table border='1'><tr><th>Id</th><th>Name</th><th>agey</th><th>doctor_id</th><br/>";
 while($row = $rseult1->fetch_assoc()){
     echo"<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>{$row['age']}</td><td>{$row['doctor_id']}</td></tr>";
 }
